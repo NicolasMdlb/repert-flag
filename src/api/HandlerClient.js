ae9408598638b6a5929415963fc3ab04208e34ac
Nicolas Mdlb <nicolas.mdlb@gmail.com>
2022-09-24 23:41:06 +0200

    session storage /local

--- src/api/HandlerClient.js
+++ src/api/HandlerClient.js
@@ -3,31 +3,30 @@ import Country from "../class/Country";
 import CountryFull from './../class/CountryFull';
 
 export async function getCountries() {
-  if (!localStorage.getItem('Countries')) {
+  if (!sessionStorage.getItem('Countries')) {
     let response = await ApiCountries.get("/all");
-    localStorage.setItem('toto', 'Je suis Toto et je fais plop')
-    localStorage.setItem('Countries', JSON.stringify(transformToCountries(response.data)))
+    sessionStorage.setItem('Countries', JSON.stringify(transformToCountries(response.data)))
   }
 
-  return JSON.parse(localStorage.getItem('Countries'));
+  return JSON.parse(sessionStorage.getItem('Countries'));
 }
 
 export async function getRegions(region) {
-  if (!localStorage.getItem(region)) {
+  if (!sessionStorage.getItem(region)) {
     let response = await ApiCountries.get(`/region/${region}`);
-    localStorage.setItem(region, JSON.stringify(transformToCountries(response.data)))
+    sessionStorage.setItem(region, JSON.stringify(transformToCountries(response.data)))
   }
 
-  return JSON.parse(localStorage.getItem(region));
+  return JSON.parse(sessionStorage.getItem(region));
 }
 
 export async function getCountry(name) {
-  if (!localStorage.getItem(name)) {
+  if (!sessionStorage.getItem(name)) {
     let response = await ApiCountries.get(`/name/${name}?fullText=true`);
-    localStorage.setItem(name, JSON.stringify(new CountryFull(response.data[0])))
+    sessionStorage.setItem(name, JSON.stringify(new CountryFull(response.data[0])))
   }
 
-  return JSON.parse(localStorage.getItem(name));
+  return JSON.parse(sessionStorage.getItem(name));
 }
 
 const transformToCountries = (data) => {
